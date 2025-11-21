8488a2e68b4748ea5c74d36e37835bf420d83c05
Kishan <mskishan26@gmail.com>
2025-11-21 04:42:59 -0500

    readme changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mskishan26.github.io',
-  base: '/my-repo',
+
   vite: {
     plugins: [tailwindcss()],
   },
